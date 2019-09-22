488c12cf6e7d937965b9096de37480a66c62ca4c
Safery <Safery@Saferys-MacBook-Pro.local>
2019-09-22 02:51:24 -0400

    asdasd

--- src/Tabs/Upload.js
+++ src/Tabs/Upload.js
@@ -17,63 +17,26 @@ const options = {
     }
   };
 
+  //<Image source={logo} style={styles.logo} />
+
 class Upload extends Component {
 
     render(){
         return (
           <View style={styles.container}>
-            <Image source={logo} style={styles.logo} />
-            <TextInput
-              placeholder="Email"
-              style={styles.input}
-            />
-            <TextInput
-              placeholder="Username"
-              style={styles.input}
-            />
-            <TextInput
-              placeholder="Password"
-              style={styles.input}
-            />
-            <TextInput
-              placeholder="Confirm Password"
-              style={styles.input}
-            />
-    </View>
+      
+          </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#4c69a5',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    height: 50,
-    backgroundColor: '#fff',
-    marginHorizontal: 10,
-    marginVertical: 5,
-   // paddingVertical: 5,
-    // paddingHorizontal: 15,
-    width: window.width - 30,
-  },
-  logo: {
-    height: IMAGE_HEIGHT,
-    resizeMode: 'contain',
-    marginBottom: 20,
-    padding:10,
-    marginTop:20
-  },
-  register:{
-    marginBottom:20, 
-    width:window.width -100,
-    alignItems:'center',
-    justifyContent:'center',
-    height:50,
-    backgroundColor: '#ffae',}
+
 });
 
 export default Upload;
